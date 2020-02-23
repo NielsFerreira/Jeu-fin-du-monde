3c244848a33c9c02e95117df3b30564a1376aad1
Niels FERREIRA <ferreiraniels@gmail.com>
2020-02-23 13:03:23 +0100

    début du jeu
    choix du jeu ainsi que le lore

---
+++ jeu.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //Ne pas oublier d'inclure le fichier time.h
+
+
+/*  										Histoire et déroulement du jeu :
+ 
+Le monde est sur sa fin. Vous êtes un survivant d'une maladie rare qui a touché la population terrestre.
+La population "vivante" a diminué de 95% suite à cette maladie. Vous voyagez et découvrez une ville campagnarde fantôme.
+Vous décidez de trouver de quoi vous sustenter dans une maison proche. Vous trouvez un coffre et décidez de prendre ou
+pas son contenu. Une fois remonté, un "mort" vous surprend et tente de vous manger. Vous avez le choix entre l'affronter
+ou vous enfuir. Ensuite, si vous êtes toujours en vie, vous allez chercher ailleurs. Vous tombez sur une cave un peu plus loin.
+Ca fait longtemps que vous n'avez pas bu d'alcool et cela vous manque. Vous décidez de descendre à la cave. Vous découvrez 
+des bouteilles poussiéreuses mais qui ont l'air intéressantes. Vous trouvez aussi une arme et des munitions.
+De la poussière tombe du dessus, vous sentez des tremblements, ainsi que du bruit venant de l'extérieur.
+Vous remontez. Votre rythme cardiaque augmente fortement quand vous voyez une horde phénoménale de morts se dirigeant vers
+votre position. Vous vous enfermez et attendez que la horde passe. Après un moment, vous n'entendez plus de bruits à l'extérieur
+et les tremblements ont cessé. Vous remontez. vous regardez autour de vous et ne voyez aucun danger apparent. Vous commencez à
+marcher quand soudain, des bruits rauques se font entendre sur votre droite. Vous voyez 2 morts, qui vout ont aussi repéré.
+L'un des morts possède un sac et fait tomber des cartouches de munitions semblables à celles que vous possédez actuellement.
+Vous réfléchissez, vous décidez soit de les affronter, soit de fuir. (affrontement face aux 2 zombies réussi, fuir vous emmène dans une
+situation plus compliquée (plus de zombies)). A la suite de ça, n'ayant pas passé un agréable moment vous décidez de quitter
+cette ville. Vous voyez un vélo et une voiture à la sortie de la ville, vous choisissez quoi prendre (Le vélo est un moyen de locomotion
+durable, qui implique pas mal d'énergie mais est tout terrain. Déplacements silencieux aussi. La voiture permet de parcourir de 
+grandes distances rapidement mais est plutôt bruyante ce qui risque d'attirer plus de morts. Cependant, la voiture permet de 
+transporter un plus grand nombre de matériel). A la suite du choix, si vous avez pris le vélo, vous crevez un pneu et vous faites
+poursuivre par des zombies en plus de ça, vous n'avez plus beaucoup d'énergie. Si vous avez pris la voiture, vous câlez plus loin
+et alertez un grand groupe de zombies, vos chances de fuite seront moins grandes que les précédentes.
+
+Fin du jeu.
+*/
+int main(){
+
+
+}
